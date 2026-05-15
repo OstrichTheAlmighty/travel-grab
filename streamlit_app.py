efa8205bda11c6843dc85e5344855b704eb408da
Nolan Watts <nolan@okwatts.com>
2026-05-14 23:07:13 -0700

    fix Streamlit import paths

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,13 @@ import os
 import sys
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
+FRONTEND = os.path.join(ROOT, "frontend")
+
 if ROOT not in sys.path:
     sys.path.insert(0, ROOT)
+
+if FRONTEND not in sys.path:
+    sys.path.insert(0, FRONTEND)
 
 from frontend.app import main
 
